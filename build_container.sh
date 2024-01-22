6e0d8db1063ff5058ff7991b999c4bbbe27a27a0
Andreas <Andreas.Zachariae@gmail.com>
2024-01-22 16:18:22 +0100

    DOmain id was too high

--- build_container.sh
+++ build_container.sh
@@ -2,5 +2,5 @@
 uid=$(eval "id -u")
 gid=$(eval "id -g")
 # --no-cache 
-docker build --build-arg UID="$uid" --build-arg GID="$gid" --build-arg ROS_DISTRO=humble --build-arg DOMAIN_ID=666 -t iras/aip_bringup:humble .
+docker build --build-arg UID="$uid" --build-arg GID="$gid" --build-arg ROS_DISTRO=humble --build-arg DOMAIN_ID=66 -t iras/aip_bringup:humble .
 
