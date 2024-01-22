d20f6df6856bb69244bd5e8369403e70cbd533f1
aip <aip@h-ka.de>
2024-01-22 13:38:10 +0100

    Update submodule

--- build_container.sh
+++ build_container.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 uid=$(eval "id -u")
 gid=$(eval "id -g")
-docker build --no-cache --build-arg UID="$uid" --build-arg GID="$gid" --build-arg ROS_DISTRO=humble --build-arg DOMAIN_ID=666 -t iras/aip_bringup:humble .
+# --no-cache 
+docker build --build-arg UID="$uid" --build-arg GID="$gid" --build-arg ROS_DISTRO=humble --build-arg DOMAIN_ID=666 -t iras/aip_bringup:humble .
 
