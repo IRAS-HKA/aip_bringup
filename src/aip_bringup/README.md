b2753bcdf5996e3400ea11abc964f2a9cfe7f046
w02_aip <email>
2024-04-16 13:08:50 +0200

    Restructure folders for demo

---
+++ src/aip_bringup/README.md
@@ -0,0 +1,6 @@
+# AIP Bringup
+
+This includes only the launch files and configs necessary to launch the AIP Demo.
+
+## How to start
+
--- src/r2e_demos/r2e_demos/build/manipulation_tasks/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/robot/ros_ws/src/r2e_demos/r2e_demos/install/manipulation_tasks'
